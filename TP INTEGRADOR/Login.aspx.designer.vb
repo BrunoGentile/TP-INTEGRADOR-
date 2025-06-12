9714ee81d83f6ed009390a2e4c0c52e18bfdbf3b
NicolasPiccioni <Nicolas_Piccioni@hotmail.com>
2025-06-12 19:07:38 -0300

    Desarrolla Interfaz Gráfica "Login" y añade interfaces de "Admin" y "Médico"

---
+++ TP INTEGRADOR/Login.aspx.designer.vb
@@ -0,0 +1,186 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class Login
+
+    '''<summary>
+    '''Control form1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''Control DIV_Login.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents DIV_Login As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+
+    '''<summary>
+    '''Control LBL_Title.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LBL_Title As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control TB_UserName.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TB_UserName As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control TB_Password.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents TB_Password As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control DIV_InterfazAdmin.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents DIV_InterfazAdmin As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+
+    '''<summary>
+    '''Control LBL_Admin_UserName.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LBL_Admin_UserName As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control HL_AltaPaciente.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents HL_AltaPaciente As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''Control HL_ModificarPaciente.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents HL_ModificarPaciente As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''Control HL_BajaPaciente.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents HL_BajaPaciente As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''Control HL_AltaMedico.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents HL_AltaMedico As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''Control HL_ModificarMedico.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents HL_ModificarMedico As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''Control HL_BajaMedico.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents HL_BajaMedico As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''Control HL_AsignarTurnos.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents HL_AsignarTurnos As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''Control HL_Informe1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents HL_Informe1 As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''Control HL_Informe2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents HL_Informe2 As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''Control DIV_InterfazMedico.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents DIV_InterfazMedico As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+
+    '''<summary>
+    '''Control LBL_Medico_UserName.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LBL_Medico_UserName As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control HL_TurnosAsignados.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents HL_TurnosAsignados As Global.System.Web.UI.WebControls.HyperLink
+End Class
